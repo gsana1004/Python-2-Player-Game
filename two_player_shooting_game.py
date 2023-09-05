eabbe91af71499051b999a7e28c809b953e7a304
GabrielSanandaji <gsana1004@gmail.com>
2023-09-05 15:12:20 -0400

    base

--- two_player_shooting_game.py
+++ two_player_shooting_game.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 import random
-import time
 
 # Initialize Pygame
 pygame.init()
@@ -9,245 +8,299 @@ pygame.init()
 # Constants
 WIDTH, HEIGHT = 800, 600
 PLAYER_SIZE = 50
-ENEMY_SIZE = 40
 BULLET_SIZE = 10
-POWERUP_SIZE = 30
+ENEMY_SIZE = 30
+POWERUP_SIZE = 20
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
-
-# Create a window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Competitive Two-Player Shooting Game")
-
-# Players
-player1_x = WIDTH // 4 - PLAYER_SIZE // 2
-player2_x = 3 * WIDTH // 4 - PLAYER_SIZE // 2
-player_y = HEIGHT - 2 * PLAYER_SIZE
-player_speed = 5
-player1 = pygame.Rect(player1_x, player_y, PLAYER_SIZE, PLAYER_SIZE)
-player2 = pygame.Rect(player2_x, player_y, PLAYER_SIZE, PLAYER_SIZE)
-
-# Bullets
-bullets1 = []
-bullets2 = []
-bullet_speed = 7
-
-# Enemies
-enemies = []
-enemy_speed = 2
-enemy_spawn_delay = 1000  # in milliseconds
-last_enemy_spawn_time = 0
-
-# Power-ups
-powerups = []
-powerup_spawn_delay = 5000  # in milliseconds
-last_powerup_spawn_time = 0
-
-# Game variables
-score1 = 0
-score2 = 0
-lives1 = 3
-lives2 = 3
-level = 1
-font = pygame.font.Font(None, 36)
-
-# Power-up types
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+PLAYER_SPEED = 5
+BULLET_SPEED = 5
+ENEMY_SPEED = 3
+ENEMY_SPAWN_INTERVAL = 1000
+POWERUP_SPAWN_INTERVAL = 5000
 POWERUP_FIRE_RATE = "fire_rate"
 POWERUP_SHIELD = "shield"
 
-# Player state
+# Create a window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Two-Player Shooting Game")
+
+# Fonts
+font = pygame.font.Font(None, 36)
+
+# Player 1
+player1 = pygame.Rect(WIDTH // 4, HEIGHT - 2 * PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE)
+player1_speed_x = 0
+player1_speed_y = 0
+player1_bullets = []
+player1_fire_rate = 1  # Bullet firing rate multiplier
 player1_shielded = False
-player2_shielded = False
 player1_shield_end_time = 0
+player1_lives = 3
+player1_score = 0
+
+# Player 2
+player2 = pygame.Rect(3 * WIDTH // 4, HEIGHT - 2 * PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE)
+player2_speed_x = 0
+player2_speed_y = 0
+player2_bullets = []
+player2_fire_rate = 1  # Bullet firing rate multiplier
+player2_shielded = False
 player2_shield_end_time = 0
+player2_lives = 3
+player2_score = 0
+
+# Enemies
+enemies = []
+
+# Power-ups
+powerups = []
+
+# Game variables
+level = 1
+last_enemy_spawn_time = 0
+last_powerup_spawn_time = 0
+game_over = False
+
+def draw_text(text, x, y, color):
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, (x, y))
+
+def reset_player(player):
+    player.x = WIDTH // 4 if player == player1 else 3 * WIDTH // 4
+    player.y = HEIGHT - 2 * PLAYER_SIZE
+    player_speed_x = 0
+    player_speed_y = 0
+    if player == player1:
+        player1_bullets.clear()
+    else:
+        player2_bullets.clear()
+    player_fire_rate = 1
+    player_shielded = False
+    player_shield_end_time = 0
 
 # Game loop
 clock = pygame.time.Clock()
-game_over = False
-winner = None  # Stores the winner when the game is over
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    # Player 1 controls
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player1_speed_x = -PLAYER_SPEED
+    elif keys[pygame.K_d]:
+        player1_speed_x = PLAYER_SPEED
+    else:
+        player1_speed_x = 0
 
-    # Player 1 controls (arrow keys to move, space to shoot)
-    if keys[pygame.K_LEFT] and player1.x > 0:
-        player1.x -= player_speed
-    if keys[pygame.K_RIGHT] and player1.x < WIDTH // 2 - PLAYER_SIZE:
-        player1.x += player_speed
+    if keys[pygame.K_w]:
+        player1_speed_y = -PLAYER_SPEED
+    elif keys[pygame.K_s]:
+        player1_speed_y = PLAYER_SPEED
+    else:
+        player1_speed_y = 0
 
     if keys[pygame.K_SPACE]:
-        if len(bullets1) < 5:  # Limit bullet count
-            bullet = pygame.Rect(player1.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2, player1.y, BULLET_SIZE, BULLET_SIZE)
-            bullets1.append(bullet)
+        if len(player1_bullets) < 5:
+            bullet = pygame.Rect(
+                player1.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2,
+                player1.y,
+                BULLET_SIZE, BULLET_SIZE)
+            player1_bullets.append(bullet)
 
-    # Player 2 controls (WASD to move, Enter/Return to shoot)
-    if keys[pygame.K_a] and player2.x > WIDTH // 2:
-        player2.x -= player_speed
-    if keys[pygame.K_d] and player2.x < WIDTH - PLAYER_SIZE:
-        player2.x += player_speed
+    # Player 2 controls
+    if keys[pygame.K_LEFT]:
+        player2_speed_x = -PLAYER_SPEED
+    elif keys[pygame.K_RIGHT]:
+        player2_speed_x = PLAYER_SPEED
+    else:
+        player2_speed_x = 0
+
+    if keys[pygame.K_UP]:
+        player2_speed_y = -PLAYER_SPEED
+    elif keys[pygame.K_DOWN]:
+        player2_speed_y = PLAYER_SPEED
+    else:
+        player2_speed_y = 0
 
     if keys[pygame.K_RETURN]:
-        if len(bullets2) < 5:  # Limit bullet count
-            bullet = pygame.Rect(player2.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2, player2.y, BULLET_SIZE, BULLET_SIZE)
-            bullets2.append(bullet)
+        if len(player2_bullets) < 5:
+            bullet = pygame.Rect(
+                player2.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2,
+                player2.y,
+                BULLET_SIZE, BULLET_SIZE)
+            player2_bullets.append(bullet)
+
+    # Update player positions
+    player1.x += player1_speed_x
+    player1.y += player1_speed_y
+    player2.x += player2_speed_x
+    player2.y += player2_speed_y
+
+    # Ensure players stay within screen boundaries
+    player1.x = max(0, min(player1.x, WIDTH - PLAYER_SIZE))
+    player1.y = max(0, min(player1.y, HEIGHT - 2 * PLAYER_SIZE))
+    player2.x = max(0, min(player2.x, WIDTH - PLAYER_SIZE))
+    player2.y = max(0, min(player2.y, HEIGHT - 2 * PLAYER_SIZE))
 
     # Spawn enemies
     current_time = pygame.time.get_ticks()
-    if current_time - last_enemy_spawn_time > enemy_spawn_delay:
+    if current_time - last_enemy_spawn_time > ENEMY_SPAWN_INTERVAL:
         enemy_x = random.randint(0, WIDTH - ENEMY_SIZE)
         enemy = pygame.Rect(enemy_x, 0, ENEMY_SIZE, ENEMY_SIZE)
         enemies.append(enemy)
         last_enemy_spawn_time = current_time
 
     # Spawn power-ups
-    if current_time - last_powerup_spawn_time > powerup_spawn_delay:
+    if current_time - last_powerup_spawn_time > POWERUP_SPAWN_INTERVAL:
         powerup_x = random.randint(0, WIDTH - POWERUP_SIZE)
         powerup_type = random.choice([POWERUP_FIRE_RATE, POWERUP_SHIELD])
         powerup = pygame.Rect(powerup_x, 0, POWERUP_SIZE, POWERUP_SIZE)
         powerups.append((powerup, powerup_type))
         last_powerup_spawn_time = current_time
 
-    # Move bullets and enemies
-    for bullet in bullets1:
-        bullet.y -= bullet_speed
-        if bullet.y < 0:
-            bullets1.remove(bullet)
+    # Move and update bullets
+    player1_bullets = [bullet for bullet in player1_bullets if bullet.y > 0]
+    player2_bullets = [bullet for bullet in player2_bullets if bullet.y > 0]
 
-    for bullet in bullets2:
-        bullet.y -= bullet_speed
-        if bullet.y < 0:
-            bullets2.remove(bullet)
+    for bullet in player1_bullets:
+        bullet.y -= BULLET_SPEED
 
+    for bullet in player2_bullets:
+        bullet.y -= BULLET_SPEED
+
+    # Move and update enemies
+    enemies = [enemy for enemy in enemies if enemy.y < HEIGHT]
     for enemy in enemies:
-        enemy.y += enemy_speed
-        if enemy.y > HEIGHT:
-            enemies.remove(enemy)
+        enemy.y += ENEMY_SPEED
 
-    for powerup, powerup_type in powerups:
-        powerup.y += enemy_speed
-        if powerup.y > HEIGHT:
-            powerups.remove((powerup, powerup_type))
-
-    # Check for collisions
-    for bullet in bullets1:
+    # Check for collisions between bullets and enemies
+    for bullet in player1_bullets:
         for enemy in enemies:
             if bullet.colliderect(enemy):
-                bullets1.remove(bullet)
+                player1_score += 1
+                player1_bullets.remove(bullet)
                 enemies.remove(enemy)
-                score1 += 1
 
-    for bullet in bullets2:
+    for bullet in player2_bullets:
         for enemy in enemies:
             if bullet.colliderect(enemy):
-                bullets2.remove(bullet)
+                player2_score += 1
+                player2_bullets.remove(bullet)
                 enemies.remove(enemy)
-                score2 += 1
 
+    # Check for collisions between players and enemies
     for enemy in enemies:
-        if enemy.colliderect(player1) and not player1_shielded:
-            lives1 -= 1
-            enemies.remove(enemy)
-        if enemy.colliderect(player2) and not player2_shielded:
-            lives2 -= 1
+        if player1.colliderect(enemy):
+            if not player1_shielded:
+                player1_lives -= 1
+                reset_player(player1)
             enemies.remove(enemy)
 
-    for powerup, powerup_type in powerups:
-        if powerup.colliderect(player1):
+        if player2.colliderect(enemy):
+            if not player2_shielded:
+                player2_lives -= 1
+                reset_player(player2)
+            enemies.remove(enemy)
+
+    # Check for collisions between players and power-ups
+    powerups = [(powerup, powerup_type) for (powerup, powerup_type) in powerups if powerup.y < HEIGHT]
+    for (powerup, powerup_type) in powerups:
+        powerup.y += 1  # Move the power-up downward
+
+        if player1.colliderect(powerup):
             if powerup_type == POWERUP_FIRE_RATE:
-                bullet_speed += 1
+                player1_fire_rate = 2
             elif powerup_type == POWERUP_SHIELD:
                 player1_shielded = True
                 player1_shield_end_time = current_time + 5000  # Shield lasts for 5 seconds
             powerups.remove((powerup, powerup_type))
 
-        if powerup.colliderect(player2):
+        if player2.colliderect(powerup):
             if powerup_type == POWERUP_FIRE_RATE:
-                bullet_speed += 1
+                player2_fire_rate = 2
             elif powerup_type == POWERUP_SHIELD:
                 player2_shielded = True
                 player2_shield_end_time = current_time + 5000  # Shield lasts for 5 seconds
             powerups.remove((powerup, powerup_type))
 
-    # Check if shields have expired
-    if current_time > player1_shield_end_time:
-        player1_shielded = False
-    if current_time > player2_shield_end_time:
-        player2_shielded = False
+    # Check for power-up expiration
+    if player1_fire_rate == 2 and current_time - player1_shield_end_time > 0:
+        player1_fire_rate = 1
+    if player2_fire_rate == 2 and current_time - player2_shield_end_time > 0:
+        player2_fire_rate = 1
 
-    # Check for level completion
-    if len(enemies) == 0:
-        level += 1
-        enemy_spawn_delay -= 100  # Make enemies spawn faster
-        last_enemy_spawn_time = current_time
+    # Check for game over
+    if player1_lives <= 0 or player2_lives <= 0:
+        game_over = True
 
     # Draw everything
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, RED, player1)
+
+    pygame.draw.rect(screen, GREEN, player1)
     pygame.draw.rect(screen, BLUE, player2)
 
-    for bullet in bullets1:
-        pygame.draw.rect(screen, WHITE, bullet)
+    for bullet in player1_bullets:
+        pygame.draw.rect(screen, RED, bullet)
 
-    for bullet in bullets2:
-        pygame.draw.rect(screen, WHITE, bullet)
+    for bullet in player2_bullets:
+        pygame.draw.rect(screen, RED, bullet)
 
     for enemy in enemies:
         pygame.draw.rect(screen, WHITE, enemy)
 
-    for powerup, powerup_type in powerups:
+    for (powerup, powerup_type) in powerups:
         if powerup_type == POWERUP_FIRE_RATE:
-            pygame.draw.rect(screen, GREEN, powerup)
+            pygame.draw.rect(screen, (255, 165, 0), powerup)
         elif powerup_type == POWERUP_SHIELD:
             pygame.draw.rect(screen, BLUE, powerup)
 
-    # Display scores, lives, and level
-    score1_text = font.render(f"Player 1 Score: {score1}", True, RED)
-    score2_text = font.render(f"Player 2 Score: {score2}", True, BLUE)
-    lives1_text = font.render(f"Player 1 Lives: {lives1}", True, RED)
-    lives2_text = font.render(f"Player 2 Lives: {lives2}", True, BLUE)
-    level_text = font.render(f"Level: {level}", True, WHITE)
-    screen.blit(score1_text, (10, 10))
-    screen.blit(score2_text, (WIDTH // 2 + 10, 10))
-    screen.blit(lives1_text, (10, 50))
-    screen.blit(lives2_text, (WIDTH // 2 + 10, 50))
-    screen.blit(level_text, (10, 90))
-
-    # Display shields (if active)
-    if player1_shielded:
-        pygame.draw.rect(screen, GREEN, pygame.Rect(player1.x - 5, player1.y - 5, PLAYER_SIZE + 10, PLAYER_SIZE + 10), 3)
-
-    if player2_shielded:
-        pygame.draw.rect(screen, GREEN, pygame.Rect(player2.x - 5, player2.y - 5, PLAYER_SIZE + 10, PLAYER_SIZE + 10), 3)
-
-    # Check for game over
-    if lives1 <= 0 or lives2 <= 0:
-        if score1 > score2:
-            winner = "Player 1 Wins!"
-        elif score2 > score1:
-            winner = "Player 2 Wins!"
-        else:
-            winner = "It's a Tie!"
-
-        game_over = True
+    draw_text(f"Player 1 Lives: {player1_lives}", 10, 10, WHITE)
+    draw_text(f"Player 2 Lives: {player2_lives}", 10, 50, WHITE)
+    draw_text(f"Player 1 Score: {player1_score}", WIDTH // 2 - 100, 10, WHITE)
+    draw_text(f"Player 2 Score: {player2_score}", WIDTH // 2 - 100, 50, WHITE)
+    draw_text(f"Level: {level}", WIDTH - 100, 10, WHITE)
 
     pygame.display.flip()
-    clock.tick(60)  # 60 FPS
+    clock.tick(60)
 
+# Game over screen
 while game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = False
+            pygame.quit()
+            sys.exit()
 
-    # Display the winner
-    winner_text = font.render(winner, True, WHITE)
-    screen.blit(winner_text, (WIDTH // 2 - 100, HEIGHT // 2 - 20))
+    screen.fill((0, 0, 0))
+    winner_text = "Player 1 Wins!" if player1_score > player2_score else "Player 2 Wins!" if player2_score > player1_score else "It's a Tie!"
+    draw_text(f"Game Over - {winner_text}", WIDTH // 2 - 200, HEIGHT // 2 - 50, WHITE)
+    draw_text(f"Player 1 Score: {player1_score}", WIDTH // 2 - 100, HEIGHT // 2, WHITE)
+    draw_text(f"Player 2 Score: {player2_score}", WIDTH // 2 - 100, HEIGHT // 2 + 40, WHITE)
+    draw_text("Press 'C' to Continue   Press 'Q' to Quit", WIDTH // 2 - 200, HEIGHT // 2 + 80, WHITE)
     pygame.display.flip()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_c]:
+        # Reset game state and continue
+        if player1_lives <= 0:
+            reset_player(player1)
+        if player2_lives <= 0:
+            reset_player(player2)
+        enemies.clear()
+        powerups.clear()
+        level = 1
+        last_enemy_spawn_time = 0
+        last_powerup_spawn_time = 0
+        game_over = False
+
+    elif keys[pygame.K_q]:
+        pygame.quit()
+        sys.exit()
 
 pygame.quit()
 sys.exit()
