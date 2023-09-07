9cea51b97c42324e61f102d0d0f9b76f3f9788c2
GabrielSanandaji <gsana1004@gmail.com>
2023-09-07 14:14:23 -0400

    close

--- game.py
+++ game.py
@@ -1,19 +1,23 @@
 import pygame
 import sys
+import cv2
+import numpy as np
 import random
+import math
 
 # Initialize Pygame
 pygame.init()
+pygame.mixer.init()
+shoot_sound = pygame.mixer.Sound("/Users/gabrielsanandaji/pythongame/assets/laser-gun-shot.wav")
 
 # Constants
-
 info_object = pygame.display.Info()
 WIDTH, HEIGHT = int(info_object.current_w * 0.98), int(info_object.current_h * 0.8)
 PLAYER_SIZE = int(WIDTH * 0.03)
 BULLET_SIZE = int(WIDTH * 0.006)
-ENEMY_SIZE = int(WIDTH * 0.02)
+ENEMY_SIZE = int(WIDTH * 0.04)
 POWERUP_SIZE = int(WIDTH * 0.01)
-WHITE = (255, 255, 255)
+WHITE = (255, 255, 255, 128)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -22,24 +26,51 @@ BULLET_SPEED = int(WIDTH * 0.0028)
 ENEMY_SPEED = int(WIDTH * 0.0017)
 ENEMY_SPAWN_INTERVAL = 1000
 POWERUP_SPAWN_INTERVAL = 5000
+# Constants for power-ups
 POWERUP_FIRE_RATE = "fire_rate"
 POWERUP_SHIELD = "shield"
+POWERUP_SIZE = int(WIDTH * 0.01)
+POWERUP_SPAWN_INTERVAL = 10  # Set the interval at which power-ups spawn (in milliseconds)
+
+
 
 # Overheating Bar Constants
-OVERHEAT_MAX = 200  # Maximum overheating value
-OVERHEAT_COOLDOWN = 3000  # Cooldown duration in milliseconds (3 seconds)
-OVERHEAT_DECREMENT = 10  # Rate at which overheating decreases per frame
+OVERHEAT_MAX = 200
+OVERHEAT_COOLDOWN = 3000
+OVERHEAT_DECREMENT = 10
 
 # Player Health Constants
 PLAYER_MAX_HEALTH = 100
-HEALTH_REGEN_RATE = 0.5  # Health regeneration rate per second
+HEALTH_REGEN_RATE = 0.5
 
 # Create a window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Two-Player Shooting Game")
 
-# Fonts
+# Load the video
+video_capture = cv2.VideoCapture('/Users/gabrielsanandaji/pythongame/BackgroundVideo.mp4')
+
+# Create a clock object to control frame rate
+clock = pygame.time.Clock()
+
+# Initialize the Xbox controller
+joystick = None
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+# Load images
+player1_img = pygame.image.load("assets/spaceship-1.png").convert_alpha()
+player1_img = pygame.transform.scale(player1_img, (PLAYER_SIZE, PLAYER_SIZE))
+player2_img = pygame.image.load("assets/spaceship-2.png")
+player2_img = pygame.transform.scale(player2_img, (PLAYER_SIZE, PLAYER_SIZE))
+enemy_img = pygame.image.load("assets/enemy-ship.png")
+enemy_img = pygame.transform.scale(enemy_img, (ENEMY_SIZE, ENEMY_SIZE))
+
+
+    # Fonts
 font = pygame.font.Font(None, int(WIDTH * 0.02))
+
 
 # Player 1
 player1_img = pygame.image.load("assets/spaceship-1.png")
@@ -60,7 +91,8 @@ player1_overheat_cooldown = 0
 player1_can_shoot = True  # Indicates if player 1 can shoot
 player1_last_shot_time = 0
 fire_rate_delay = 500
-
+player1_rotation = 0  # Initial rotation angle for player 1
+player1_img_rotated = player1_img  # Initialize the rotated image
 # Player 2
 player2_img = pygame.image.load("assets/spaceship-2.png")
 player2_img = pygame.transform.scale(player2_img, (PLAYER_SIZE, PLAYER_SIZE))
@@ -79,14 +111,23 @@ player2_overheat = 0
 player2_overheat_cooldown = 0
 player2_can_shoot = True  # Indicates if player 2 can shoot
 player2_last_shot_time = 0
-
+player2_rotation = 0  # Initial rotation angle for player 2
+player2_img_rotated = player2_img  # Initialize the rotated image
 # Enemies
-enemy_img = pygame.image.load("assets/enemy-ship.jpg")
+enemy_img = pygame.image.load("assets/enemy-ship.png")
 enemy_img = pygame.transform.scale(enemy_img, (ENEMY_SIZE, ENEMY_SIZE))
 enemies = []
 
+bullet_img = pygame.image.load("/Users/gabrielsanandaji/pythongame/assets/output-onlinepngtools (1).png").convert_alpha()
+bullet_img = pygame.transform.scale(bullet_img, (BULLET_SIZE, BULLET_SIZE))
 # Power-ups
 powerups = []
+def spawn_powerup():
+    powerup_type = random.choice([POWERUP_FIRE_RATE, POWERUP_SHIELD])
+    powerup_x = random.randint(0, WIDTH - POWERUP_SIZE)
+    powerup_y = -POWERUP_SIZE  # Start power-ups above the screen
+    powerup_rect = pygame.Rect(powerup_x, powerup_y, POWERUP_SIZE, POWERUP_SIZE)
+    return powerup_rect, powerup_type
 
 # Game variables
 level = 1
@@ -127,8 +168,40 @@ def draw_health_bar(player_health, player_x, player_y):
     pygame.draw.rect(screen, GREEN, health_bar_rect)
     pygame.draw.rect(screen, WHITE, (player_x, player_y - 10, bar_width, 5), 1)
 
+
+
 # Game loop
 clock = pygame.time.Clock()
+current_time = pygame.time.get_ticks()
+
+# Spawn power-ups
+if current_time - last_powerup_spawn_time > POWERUP_SPAWN_INTERVAL:
+    powerup_rect, powerup_type = spawn_powerup()
+    powerups.append((powerup_rect, powerup_type))
+    last_powerup_spawn_time = current_time
+
+# Update power-up positions
+powerups = [(powerup, powerup_type) for (powerup, powerup_type) in powerups if powerup.y < HEIGHT]
+for (powerup, _) in powerups:
+    powerup.y += ENEMY_SPEED  # Adjust the speed as needed
+
+# Check for collisions between players and power-ups
+for (powerup, powerup_type) in powerups:
+    if player1.colliderect(powerup):
+        if powerup_type == POWERUP_FIRE_RATE:
+            player1_fire_rate = 2
+        elif powerup_type == POWERUP_SHIELD:
+            player1_shielded = True
+            player1_shield_end_time = 250  # Shield lasts for 5 seconds
+        powerups.remove((powerup, powerup_type))
+
+    if player2.colliderect(powerup):
+        if powerup_type == POWERUP_FIRE_RATE:
+            player2_fire_rate = 2
+        elif powerup_type == POWERUP_SHIELD:
+            player2_shielded = True
+            player2_shield_end_time = current_time + 5000  # Shield lasts for 5 seconds
+        powerups.remove((powerup, powerup_type))
 
 while not game_over:
     for event in pygame.event.get():
@@ -151,17 +224,23 @@ while not game_over:
     else:
         player1_speed_y = 0
 
-    if keys[pygame.K_SPACE]:
-        if keys[pygame.K_SPACE] and player1_can_shoot:
-            current_time = pygame.time.get_ticks()
-            if current_time - player1_last_shot_time > fire_rate_delay:
-                bullet = pygame.Rect(
-                    player1.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2,
-                    player1.y,
-                    BULLET_SIZE, BULLET_SIZE)
-                player1_bullets.append(bullet)
-                player1_last_shot_time = current_time
-                player1_overheat += 10
+    if keys[pygame.K_SPACE] and player1_can_shoot:
+        current_time = pygame.time.get_ticks()
+        if current_time - player1_last_shot_time > fire_rate_delay:
+            bullet_x = player1.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2
+            bullet_y = player1.y
+            bullet = pygame.Rect(bullet_x, bullet_y, BULLET_SIZE, BULLET_SIZE)
+            player1_bullets.append(bullet)
+            player1_last_shot_time = current_time
+            player1_overheat += 10
+        # Play the shooting sound effect
+            shoot_sound.play()
+
+
+    if keys[pygame.K_LSHIFT]:  # Rotate player 1
+        player1_rotation = (player1_rotation - 90) % 360
+        player1_img_rotated = pygame.transform.rotate(player1_img, player1_rotation)
+        player1 = player1_img_rotated.get_rect(center=player1.center)
 
     # Player 2 controls
     if keys[pygame.K_LEFT]:
@@ -178,18 +257,27 @@ while not game_over:
     else:
         player2_speed_y = 0
 
-    if keys[pygame.K_RETURN]:
-        if keys[pygame.K_RETURN] and player2_can_shoot:
-            current_time = pygame.time.get_ticks()
-            if current_time - player2_last_shot_time > fire_rate_delay:
-                bullet = pygame.Rect(
-                    player2.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2,
-                    player2.y,
-                    BULLET_SIZE, BULLET_SIZE)
-                player2_bullets.append(bullet)
-                player2_last_shot_time = current_time
-                player2_overheat += 10
+    if keys[pygame.K_RETURN] and player2_can_shoot:
+        current_time = pygame.time.get_ticks()
+        if current_time - player2_last_shot_time > fire_rate_delay:
+            bullet_x = player2.x + PLAYER_SIZE // 2 - BULLET_SIZE // 2
+            bullet_y = player2.y
+            bullet = pygame.Rect(bullet_x, bullet_y, BULLET_SIZE, BULLET_SIZE)
+            player2_bullets.append(bullet)
+            player2_last_shot_time = current_time
+            player2_overheat += 10
+        # Play the shooting sound effect
+            shoot_sound.play()
 
+
+    if keys[pygame.K_RSHIFT]:  # Rotate player 2
+        player2_rotation = (player2_rotation - 90) % 360
+        player2_img_rotated = pygame.transform.rotate(player2_img, player2_rotation)
+        player2 = player2_img_rotated.get_rect(center=player2.center)
+
+    # Draw players using the rotated images
+    screen.blit(player1_img_rotated, player1)
+    screen.blit(player2_img_rotated, player2)
     # Player 1 movement
     player1.x += player1_speed_x
     player1.y += player1_speed_y
@@ -290,6 +378,16 @@ while not game_over:
                     game_over = True
                     break
             enemies.remove(enemy)
+    # Check for collisions between player bullets and players
+    for bullet in player1_bullets:
+        if bullet.colliderect(player2):
+            player2_health -= 5  # Deduct 5% health
+            player1_bullets.remove(bullet)
+
+    for bullet in player2_bullets:
+        if bullet.colliderect(player1):
+            player1_health -= 5  # Deduct 5% health
+            player2_bullets.remove(bullet)
 
     # Check for collisions between players and power-ups
     powerups = [(powerup, powerup_type) for (powerup, powerup_type) in powerups if powerup.y < HEIGHT]
@@ -328,6 +426,23 @@ while not game_over:
     # Draw everything
     screen.fill((0, 0, 0))
 
+    # Capture a frame from the video
+    ret, frame = video_capture.read()
+
+    if not ret:
+        break  # Break the loop if we've reached the end of the video
+
+    # Convert the frame to a Pygame surface
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
+    frame = np.rot90(frame)  # Rotate the frame 90 degrees
+    frame = pygame.surfarray.make_surface(frame)  # Convert to Pygame surface
+
+    # Resize the frame to match the game window dimensions
+    frame = pygame.transform.scale(frame, (WIDTH, HEIGHT))
+
+    # Blit the frame as the background
+    screen.blit(frame, (0, 0))
+
     screen.blit(player1_img, player1)
     screen.blit(player2_img, player2)
 
@@ -336,17 +451,20 @@ while not game_over:
     pygame.draw.rect(screen, WHITE, (WIDTH - 10 - player2_overheat, 10, player2_overheat, 20))
 
     for bullet in player1_bullets:
-        pygame.draw.rect(screen, RED, bullet)
+        screen.blit(bullet_img, bullet)
+
 
     for bullet in player2_bullets:
-        pygame.draw.rect(screen, BLUE, bullet)
+        screen.blit(bullet_img, bullet)
+
 
     for enemy in enemies:
         # Rotate and draw enemy images
         rotated_enemy = pygame.transform.rotate(enemy_img, enemy_rotation_angle)
+        
         screen.blit(rotated_enemy, enemy)
 
-    for (powerup, _) in powerups:
+    for (powerup, powerup_type) in powerups:
         pygame.draw.rect(screen, GREEN, powerup)
 
     # Display overheating warning for player 1
@@ -428,3 +546,10 @@ while True:  # Keep this loop running
         pygame.display.flip()
 
     clock.tick(60)
+
+    # Quit Pygame
+    pygame.quit()
+
+        # Quit OpenCV
+    video_capture.release()
+    cv2.destroyAllWindows()
